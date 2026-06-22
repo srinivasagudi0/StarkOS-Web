1c05894c5e185b6edfd5df57eb7c8aafa7ba246d
Srinivasa Gudi <239487102+srinivasagudi0@users.noreply.github.com>
2026-06-22 09:06:59 -0500

    added the firt actual page but still palceholder like and fixx issues in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,19 @@
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
-import Home from './Home'
+import CommandCenter from './command_center'
 import About from './About'
-import { use, useEffect } from 'react'
+
 
 function App() {
   
   return (
     <BrowserRouter>
       <nav>
-        <Link to="/">Command Center</Link>{' '}
+        <Link to="/">Command Center</Link>
         <Link to="/about">About</Link>
       </nav>
 
       <Routes>
-        <Route path="/" element={<command_center />} />
+        <Route path="/" element={<CommandCenter />} />
         <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
--- frontend/src/command_center.jsx
+++ frontend/src/command_center.jsx
@@ -1,1 +1,15 @@
-function About
+function CommandCenter() {
+    return (
+        <main>
+            <h1>Command Center</h1>
+            <ul>Active Missions: None</ul>
+            <ul>Streaks: None</ul>
+            <ul>Focus Score: None</ul>
+            <ul>Energy Level: None</ul>
+            <ul>Warnings: None</ul>
+            <ul>Jarvis Daily Advice: None</ul>
+        </main> 
+    )   
+}
+
+export default CommandCenter
