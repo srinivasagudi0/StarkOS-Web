59f27a9d2afd6592ddc6857d9e97fa8f84a72120
Srinivasa Gudi <239487102+srinivasagudi0@users.noreply.github.com>
2026-06-22 08:52:25 -0500

    refactor: remove unused Home component and related API calls; add command_center component placheolder

--- app.py
+++ app.py
@@ -3,16 +3,10 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 
-@app.route("/api/hello")
-def hello():
-    return jsonify({"message": "Hello, World!"})
 
-@app.route("/api/greet/<name>")
-def greet():
-    name = "hi"
-    return jsonify({"message": f"Hello, {name}!"})
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,19 @@
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 import Home from './Home'
 import About from './About'
+import { use, useEffect } from 'react'
 
 function App() {
+  
   return (
     <BrowserRouter>
       <nav>
-        <Link to="/">Home</Link>{' '}
+        <Link to="/">Command Center</Link>{' '}
         <Link to="/about">About</Link>
       </nav>
 
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<command_center />} />
         <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
