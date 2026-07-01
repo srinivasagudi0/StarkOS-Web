982e33ad1c2a882fcb2fb0d0a67da9f9cbdbf211
Srinivasa Gudi <239487102+srinivasagudi0@users.noreply.github.com>
2026-06-30 19:09:59 -0500

    added a simple box and textarea and styled it

--- frontend/src/mission_forge.jsx
+++ frontend/src/mission_forge.jsx
@@ -5,10 +5,21 @@ function MissionForge() {
       <br />
       <div className="title3">
         <h1>Mission Forge</h1>
-        </div>
+      </div>
+      <br />
+      <div className="forge-text1">
+      <h2>Beta: Quick Add (with AI)</h2>
+
+    <div className="card advice-card">
+      <p>Enter a brief description of your mission(s). AI will generate one or more mission suggestions you can accept or rejct. Editing will be implemented later.</p>
+      <textarea className="mission-input" placeholder="Enter your mission description here..."></textarea>
+      </div>
+      <br />
+    </div>
     </main>
     
   )
 }
 
 export default MissionForge;
+
