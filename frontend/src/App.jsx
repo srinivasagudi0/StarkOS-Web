923c8a6e4a1ab51fd30358b758eea11987915df4
Srinivasa Gudi <239487102+srinivasagudi0@users.noreply.github.com>
2026-07-11 10:34:35 -0500

    Add Coding Intelligence page and update navigation links

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import CommandCenter from './command_center'
 import MissionControl from './mission_control'
 import MissionForge from './mission_forge'
 //import MissionTracker from './mission_tracker'
+import Code from './coding'
 import About from './About'
 
 
@@ -14,6 +15,7 @@ function App() {
         <Link to="/">Command Center</Link>
         <Link to="/mission-control">Mission Control</Link>
         <Link to="/missions-forge">Mission Forge</Link>
+        <Link to="/coding">Coding Intelligence</Link>
         <Link to="/About">About</Link>
       </nav>
 
@@ -21,6 +23,7 @@ function App() {
         <Route path="/" element={<CommandCenter />} />
         <Route path="/mission-control" element={<MissionControl />} />
         <Route path="/missions-forge" element={<MissionForge />} />
+        <Route path="/coding" element={<Code />} />
         <Route path="/About" element={<About />} />
         
       </Routes>
---
+++ frontend/src/coding.jsx
@@ -0,0 +1,10 @@
+
+function Code() {
+    return (
+        <main>
+            <h1>Coding Intelligence</h1>
+        </main>
+    )
+}
+
+export default Code;
